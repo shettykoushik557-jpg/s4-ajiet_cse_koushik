84529106e2a75496beaef151c86c7b052fa339b6
shettykoushik557-jpg <shettykoushik557@gmail.com>
2026-05-04 12:04:11 +0530

    Create swap2.java

---
+++ swap2.java
@@ -0,0 +1,11 @@
+class Main {
+    public static void main(String[] args) {
+        int a=20;
+        int b=40;
+        a=a^b;
+        b=a^b;
+        a=a^b;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
