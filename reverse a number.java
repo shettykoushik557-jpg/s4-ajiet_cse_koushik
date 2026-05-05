b311530cdef2e9424adad06ffc81e3582d94bd21
koushik557 <shettykoushik557@gmail.com>
2026-05-05 14:01:57 +0530

    Update reverse a number.java

--- reverse a number.java
+++ reverse a number.java
@@ -1,1 +1,17 @@
-
+class Main {
+    public static void main(String[] args) {
+        int number = 2562006;=
+        int reversedNumber = 0;
+        
+        System.out.println("Original Number: " + number);
+        
+       
+        while (number != 0) {
+            int remainder = number % 10;         
+            reversedNumber = reversedNumber * 10 + remainder; 
+            number = number / 10;                 
+        }
+        
+        System.out.println("Reversed Number: " + reversedNumber);
+    }
+}
