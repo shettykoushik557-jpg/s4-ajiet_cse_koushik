25f26f2c76d7a6f5a8dd0ce4ff3d15f43ce90b04
koushik557 <shettykoushik557@gmail.com>
2026-05-05 11:26:27 +0530

    Create q3.java

---
+++ q3.java
@@ -0,0 +1,21 @@
+
+import java.util.Scanner;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        int count=0;
+        int smallest=arr[0];
+         for(int i=0;i<arr.length;i++){
+           if(arr[i]<smallest){
+               smallest=arr[i];
+               count++;
+           }
+       }
+       System.out.println(count);
+    }
+}
