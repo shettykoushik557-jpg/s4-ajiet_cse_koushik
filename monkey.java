062ae462e379f004349a88a040f57258e3021b0c
koushik557 <shettykoushik557@gmail.com>
2026-05-05 14:04:03 +0530

    Update monkey.java

--- monkey.java
+++ monkey.java
@@ -1,1 +1,26 @@
-
+import java.util.Scanner;
+class Main {
+    public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+    System.out.println("enter the value of n.k.j,m,p");
+    int n=sc.nextInt();
+    int k=sc.nextInt();
+    int j=sc.nextInt();
+    int m=sc.nextInt();
+    int p=sc.nextInt();
+    if(k==0||j==0||n==0){
+        System.out.println("the k and j and n value cannot be zero");
+    }
+    else{
+         int a=m/k;
+         int b=p/j;
+         int c=a+b;
+         int result=n-c;
+         if((m%k!=0)||(p%j!=0)) {
+             result=result-1;
+         }
+         System.out.println(result);
+         
+         }
+    }
+    
